85a310b3ec8db35eec64b763d32f12aba9070193
Sam Evans <evansst@icloud.com>
2020-06-16 06:25:19 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -46,10 +46,15 @@ def game_hash
           :blocks => 8, 
           :slam_dunks => 5},
         :jason_terry => {
-          
-        }
-          
-          
+          :player_name => "Jason Terry", 
+          :number => 31,
+          :shoe => 15, 
+          :points => 19,
+          :rebounds => 2, 
+          :assists => 2, 
+          :steals => 4, 
+          :blocks => 11,
+          :slam_dunks => 1}
       ]
     },
     :away => {
